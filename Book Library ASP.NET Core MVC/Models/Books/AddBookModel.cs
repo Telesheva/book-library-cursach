faca2abefe3601db084ca51c51adc7b993f3ec23
Telesheva <megoirlik11@gmail.com>
2021-03-22 21:55:19 +0200

    course project is done

---
+++ Book Library ASP.NET Core MVC/Models/Books/AddBookModel.cs
@@ -0,0 +1,16 @@
+﻿using Book_Library_Repository_EF_Core.Models.Book;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Book_Library_ASP.NET_Core_MVC.Models.Books
+{
+    [ModelMetadataType(typeof(ActionBookModelMetadata))]
+    public class AddBookModel : BookItem
+    {
+        public string AddBookMassege { get; set; }
+    }
+}
---
+++ Book Library Repository EF Core/Models/Book/BookItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Book_Library_Repository_EF_Core.Models.Book
+{
+    public class BookItem
+    {
+        public int? ID { get; set; }
+        public string Name { get; set; }
+        public string Authors { get; set; }
+        public DateTime Year { get; set; }
+        public bool? Availability { get; set; }
+
+        public BookItem() { }
+
+        public BookItem(BookItem bookItem) 
+        {
+            ID = bookItem.ID;
+            Name = bookItem.Name;
+            Authors = bookItem.Authors;
+            Year = bookItem.Year;
+            Availability = bookItem.Availability;
+        }
+    }
+}
